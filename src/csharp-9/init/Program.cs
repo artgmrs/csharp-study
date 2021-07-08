a4a531091c2824f4ac6b30e20603a1e02aa669ff
Arthur Guimarães <arthurdonds@gmail.com>
2021-07-08 13:44:55 -0300

    Added some csharp-9 study cases

---
+++ src/csharp-9/init/Program.cs
@@ -0,0 +1,17 @@
+﻿System.Console.WriteLine("Hello World!");
+
+Person p = new()
+{
+    Cpf = "06666600660",
+    Name = "João"
+};
+
+p.Cpf = "não pode fazer isso";
+
+System.Console.WriteLine(p.Cpf, p.Name); //error CS8852: Init-only property or indexer 'Person.Cpf' can only be assigned in an object initializer, or on 'this' or 'base' in an instance constructor or an 'init' accessor.
+
+public class Person
+{
+    public string Cpf { get; init; }
+    public string Name { get; set; }
+}
---
+++ src/csharp-9/records/Program.cs
@@ -0,0 +1,29 @@
+﻿TestRecord t = new()
+{
+    FirstName = "Arthur",
+    LastName = "Miranda"
+};
+
+//expressão 'with' com records
+TestRecord t2 = t with
+{
+    FirstName = "Carlos"
+};
+
+System.Console.WriteLine(t2); //TestRecord { FirstName = Carlos, LastName = Miranda }
+
+//positional records
+TestRecordTwo t3 = new("Lucas", "Silva");
+
+var (firstName, lastName) = t3;
+System.Console.WriteLine(firstName); //Lucas
+System.Console.WriteLine(lastName); //Silva
+
+public record TestRecord()
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
+
+//positional records
+public record TestRecordTwo(string firstName, string lastName);
